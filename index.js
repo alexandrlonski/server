a6b1fa4063d4301a1b441680c36d5c28cbfe7bc0
Alexandr <alexandr@MacBook-Pro-Alexandr.local>
2021-12-01 21:46:58 +0300

    fix: set up prettier

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const models = require('./models/models')
-const sequelize = require('./db')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const models = require("./models/models");
+const sequelize = require("./db");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express(); //create server
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
- res.status(200).json({message: "WORKING!"})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "WORKING!" });
+});
 
 const start = async () => {
-    try {
-       await sequelize.authenticate() // Connect with database
-       await sequelize.sync() // sync with database
-       app.listen(PORT, () => console.log(`Server started omn port ${PORT}`))
-    } catch (e) {
-        console.log(e);
-    }
-}
+  try {
+    await sequelize.authenticate(); // Connect with database
+    await sequelize.sync(); // sync with database
+    app.listen(PORT, () => console.log(`Server started omn port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
+start();
