2cf77780b89132aecd080d9803f9a4bb2219a7cf
Alexandr <aleksandr.lonski.86@gmail.com>
2021-12-01 12:30:03 +0300

    feat: connect to database

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
 
 const PORT = process.env.PORT || 8080;
 
 const app = express(); //create server
 
-app.listen(PORT, () => console.log(`Server started omn port ${PORT}`))
+const start = async () => {
+    try {
+       await sequelize.authenticate() // Connect with database
+       await sequelize.sync() // sync with database
+       app.listen(PORT, () => console.log(`Server started omn port ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
 
+start()
