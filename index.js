5cb40f57af6a3c73a5587e574c543e94a235ba1c
Alexandr <aleksandr.lonski.86@gmail.com>
2021-12-02 10:50:49 +0300

    feat: implement registration, login and check user

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-require('dotenv').config()
-const express = require('express')
-const models = require('./models/models')
-const sequelize = require('./db')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const models = require("./models/models");
+const sequelize = require("./db");
+const cors = require("cors");
+const router = require("./routes");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express(); //create server
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
 
-app.get('/', (req, res) => {
- res.status(200).json({message: "WORKING!"})
-})
+// Error Handling
+app.use(errorHandler);
 
 const start = async () => {
-    try {
-       await sequelize.authenticate() // Connect with database
-       await sequelize.sync() // sync with database
-       app.listen(PORT, () => console.log(`Server started omn port ${PORT}`))
-    } catch (e) {
-        console.log(e);
-    }
-}
+  try {
+    await sequelize.authenticate(); // Connect with database
+    await sequelize.sync(); // Sync with database
+    app.listen(PORT, () => console.log(`Server started omn port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
+start();
